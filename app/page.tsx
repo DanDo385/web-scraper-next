e3a51f13b90d764e09c3a101cd8543c064c36670
Daniel Magro <dando385@yahoo.com>
2023-10-18 17:16:30 -0400

    fixed favicon.ico

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Image from 'next/image'
+import HeroCarousel from "@/components/HeroCarousel"
+import Searchbar from "@/components/Searchbar"
+import Image from "next/image"
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className = "px-6 md:px-20 py-24 border-2 border-red-500">
@@ -24,11 +30,24 @@ const Home = () => {
             <p className = "mt-6">
               Powerful, self-serve product to help you track the products you care about.
             </p>
+
+            <Searchbar />
           </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className = "trending-section">
+        <h2 className = "section-text"> Trending </h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
         </div>
       </section>
     </>
-  );
+  )
 }
 
-export default Home;
+export default Home
