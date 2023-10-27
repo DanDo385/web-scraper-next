bb2c4e7e00650da9a0edd41d5e26709ec8349835
Daniel Magro <dando385@yahoo.com>
2023-10-27 17:02:14 -0400

    added brightdata user info

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,3 +1,27 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
 export async function scrapeAmazonProduct(url: string) {
     if(!url) return;
+
+    const username = String(process.env.USERNAME)
+    const password = String(process.env.PASSWORD)
+    const port = 22225;
+    const session_id = (1000000 * Math.random()) | 0;
+    const options = {
+        auth: {
+            username: `${username}-session-${session_id}`,
+            password,
+        },
+        host: 'brd.superproxy.io',
+        port,
+        rejectUnauthorized: false,
+    }
+    try {
+        // Fetch Product Page
+        const response = await axios.get(url, options); {
+    } catch (error: any) {
+        throw new Error(`Failed to scrape product: ${error.message}`)
+    }  
+
 }
